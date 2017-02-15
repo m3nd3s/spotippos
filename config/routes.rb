a3a1f8c6143cee722f1e3aa6270d28bb3b2b4a16
Almir Mendes <m3nd3s@gmail.com>
2017-02-15 17:35:34 -0200

    Add API version

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :properties
+  namespace :v1 do
+    resources :properties
+  end
 end
